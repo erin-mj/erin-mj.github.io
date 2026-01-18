7ed8f8f2e6e5a5445e8471ab3617cef886981b67
erin-mj <erin.maria.johnson@gmail.com>
2026-01-18 01:50:12 +0000

    Deploying to gh-pages from @ erin-mj/erin-mj.github.io@8eac5694bf1fff74e135829f324c887625f35ee6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,7 +38,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2026/what-anesthesia-taught-me/";
+            window.location.href = "/blog/2025/what-anesthesia-taught-me/";
           
         },
       },{id: "books-the-godfather",
